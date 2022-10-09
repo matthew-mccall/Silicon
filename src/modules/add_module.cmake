3d7c99d14b5212d926171f7c2b9c2a97e3b4eb70
Matthew McCall <matthew+github@mmccall.dev>
2022-10-09 15:36:06 -0400

    Adjusted Modules build behavior

--- src/modules/add_module.cmake
+++ src/modules/add_module.cmake
@@ -1,4 +1,4 @@
 function(add_module)
     add_library(${ARGV})
-    target_link_libraries(${MODULE_NAME} PRIVATE ${SI_DEPENDENCIES})
+    target_link_libraries(${PROJECT_NAME} PRIVATE ${SI_DEPENDENCIES})
 endfunction(add_module)
