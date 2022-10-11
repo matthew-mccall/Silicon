686587f13a8a450e6ea8520ef626137737e4199e
Matthew McCall <matthew+github@mmccall.dev>
2022-10-11 10:59:21 -0400

    Switch to custom Boost install

--- cmake/Desktop/ResolveDependencies.cmake
+++ cmake/Desktop/ResolveDependencies.cmake
@@ -1,4 +1,20 @@
 if (SI_PLATFORM STREQUAL "Desktop")
-    add_subdirectory(libs/boost-cmake)
+
+    execute_process(
+            COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/scripts/setup.sh"
+            WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/libs/boost"
+    )
+#
+#    set(FETCHCONTENT_SOURCE_DIR_BOOST "${CMAKE_CURRENT_SOURCE_DIR}/libs/boost")
+#    add_subdirectory(libs/boost-cmake)
+
+    set(BOOST_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/libs/boost")
+    set(Boost_NO_SYSTEM_PATHS ON)
+    find_package(Boost REQUIRED)
+
+    if(Boost_FOUND)
+        include_directories(${Boost_INCLUDE_DIRS})
+    endif()
+
     add_subdirectory(libs/SDL)
 endif()
