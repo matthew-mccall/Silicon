a6458ef963acfe543b2237a0ee7bea8a6304aa97
Matthew McCall <matthew+github@mmccall.dev>
2023-01-03 10:54:49 -0500

    Replace use of variables with target properties

--- cmake/Web/BuildOptions.cmake
+++ cmake/Web/BuildOptions.cmake
@@ -1,8 +1,4 @@
 if (SI_PLATFORM STREQUAL "Web")
     target_compile_options(${PROJECT_NAME} PUBLIC "-sUSE_SDL=2" "-sUSE_BOOST_HEADERS=1")
     target_link_options(${PROJECT_NAME} PUBLIC "-sUSE_SDL=2" "-sUSE_BOOST_HEADERS=1")
-
-#    foreach(MODULE IN LISTS ENGINE_MODULES)
-#        target_compile_options(${MODULE} PRIVATE "-sUSE_SDL=2" "-sUSE_BOOST_HEADERS=1")
-#    endforeach()
 endif()
--- cmake/Web/ResolveDependencies.cmake
+++ cmake/Web/ResolveDependencies.cmake
@@ -1,6 +1,4 @@
 if (SI_PLATFORM STREQUAL "Web")
-#    include(src/web/web.cmake)
-
     add_subdirectory(src/web)
     list(APPEND SI_DEPENDENCIES
          Silicon::Web
